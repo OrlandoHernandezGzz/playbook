b86da694cb793ae9e6a8046ff80cf98363eaa71a
Orlando <221299oaHG>
2022-04-23 00:45:36 -0500

    Paso 4: se practico con distintas rutas

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -10,6 +10,22 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// Agregando nueva ruta.
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a launchx')
+})
+
+// Agregando nueva ruta regresando un objeto.
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
+// Nueva ruta agregando query params.
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+}) // :explorerName indica valor enviado por url
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
