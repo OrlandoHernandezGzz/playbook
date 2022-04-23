87feb96f83dbb12012d65d34215e902a6ea8c7d0
Orlando <221299oaHG>
2022-04-23 00:01:41 -0500

    feat: termino sprint 2

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -4,6 +4,20 @@ class UserService {
     static create(id, username, name){
         return new User(id, username, name, "Sin Bio")
     }
+
+    // Métodos.
+    static getInfo(user){
+        return Object.values(user)
+    }
+
+    static updateUserUsername(user, newUsername){
+        user.username = newUsername
+    }
+
+    static getAllUsernames(user){
+        const userNames = user.map(user => user.username)
+        return userNames
+    }
 }
 
 module.exports = UserService
--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -8,4 +8,29 @@ describe("Test for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
+
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "Hernandez", "Orlando")
+        const userInfo = UserService.getInfo(user);
+        expect(userInfo[0]).toBe(1)
+        expect(userInfo[1]).toBe("Hernandez")
+        expect(userInfo[2]).toBe("Orlando")
+        expect(userInfo[3]).toBe("Sin Bio")
+    })
+
+    test("3. Update username", () => {
+        const user = UserService.create(1, "Hernandez", "Orlando")
+        UserService.updateUserUsername(user, "OrlaHdz")
+        expect(user.username).toBe("OrlaHdz")
+    })
+
+    test("4. Given a list of users give me the list of usernames", () => {
+        const user1 = UserService.create(1, "OrlaHdz1", "Orlando")
+        const user2 = UserService.create(2, "OrlaHdz2", "Orlando")
+        const user3 = UserService.create(3, "OrlaHdz3", "Orlando")
+        const usernames = UserService.getAllUsernames([user1, user2, user3])
+        expect(usernames).toContain("OrlaHdz1")
+        expect(usernames).toContain("OrlaHdz2")
+        expect(usernames).toContain("OrlaHdz3")
+    })
 })
